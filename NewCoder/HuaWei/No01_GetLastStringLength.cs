717251aa2d21814e2f75e62830277ee8752e912c
zhouyi <zhouyi9211@163.com>
2020-03-02 21:03:41 +0800

    添加代码

---
+++ NewCoder/HuaWei/No01_GetLastStringLength.cs
@@ -0,0 +1,22 @@
+﻿namespace CodePractice.NewCoder.HuaWei
+{
+    public class No01_GetLastStringLength
+    {
+        public static int GetLastStringLength(string input)
+        {
+            int length = 0;
+            bool canStart = false;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                var ch = input[i];
+                if (ch == ' ' && canStart == false)
+                    continue;
+                canStart = true;
+                if (ch == ' ')
+                    break;
+                length++;
+            }
+            return length;
+        }
+    }
+}
---
+++ NewCoder/HuaWei/No5_HexDecimalConvert.cs
@@ -0,0 +1,50 @@
+﻿namespace CodePractice.NewCoder.HuaWei
+{
+    public class No5_HexDecimalConvert
+    {
+        /// <summary>
+        /// 十六进制转化
+        /// </summary>
+        public static long HexadecimalConvert(string hexString)
+        {
+            hexString = hexString.ToLower();
+            long result = 0;
+            for (int i = 2; i < hexString.Length; i++)
+            {
+                var hexChar = hexString[i];
+                var decimalValue = HexToDecimalDigit(hexChar);
+                result = result * 16 + decimalValue;
+            }
+            return result;
+        }
+
+        private static int HexToDecimalDigit(char a)
+        {
+            if (a == 'a')
+            {
+                return 10;
+            }
+            else if (a == 'b')
+            {
+                return 11;
+            }
+            else if (a == 'c')
+            {
+                return 12;
+            }
+            else if (a == 'd')
+            {
+                return 13;
+            }
+            else if (a == 'e')
+            {
+                return 14;
+            }
+            else if (a == 'f')
+            {
+                return 15;
+            }
+            return a - '0';
+        }
+    }
+}
