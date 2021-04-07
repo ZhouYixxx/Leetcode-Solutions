c0716163fac336a5e8b276364f50110b05803f44
zhouyi <zhouyi9211@163.com>
2021-04-08 00:42:18 +0800

    2021.04.07

---
+++ LeetCode/DP/ShortestWay_1055.cs
@@ -0,0 +1,33 @@
+﻿using CodePractice.Core;
+
+namespace CodePractice.LeetCode.DP
+{
+    public class ShortestWay_1055 : LeetCodeBase
+    {
+        public ShortestWay_1055() : base("ShortestWay_1055")
+        {
+
+        }
+
+        public int ShortestWay(string source, string target)
+        {
+            int len = 0, i = 0;
+            while (i < target.Length)
+            {
+                int temp = i, j = 0;
+                //寻找target中以target.charAt(i)开头的连续子字符串所能匹配的source最长子序列
+                while (j < source.Length)
+                {
+                    if (source[j++] == target[i])
+                    {
+                        if (++i == target.Length) break;
+                    }
+                }
+                //若source中的子序列与target的子字符串不能匹配
+                if (temp == i) return -1;
+                len++;
+            }
+            return len;
+        }
+    }
+}
