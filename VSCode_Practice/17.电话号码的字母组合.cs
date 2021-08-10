03a08f24f3e2d4ac98a00530c98928239ca09af8
zhouy245 <zhouyi9211@163.com>
2021-08-10 18:31:48 +0800

    17

--- VSCode_Practice/17.电话号码的字母组合.cs
+++ VSCode_Practice/17.电话号码的字母组合.cs
@@ -6,11 +6,44 @@
 
 // @lc code=start
 using System;
+using System.Text;
 using System.Collections.Generic;
 
-public class Solution017 {
-    public IList<string> LetterCombinations(string digits) {
-        return new List<string>();
+public class Solution017 
+{
+    public void Test()
+    {
+        var digits = "2";
+        var ans = LetterCombinations(digits);
+    }
+
+    public IList<string> LetterCombinations(string digits) 
+    {
+        var ans = new List<string>();
+        if (digits.Length == 0)
+        {
+            return ans;
+        }
+        var letterDic = new Dictionary<char,string>()
+        {
+            {'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}
+        };
+        Backtrace(digits,0,letterDic,ans,"");
+        return ans;
+    }
+
+    private void Backtrace(string digits, int index, Dictionary<char,string> letterDic, List<string> result, string tempResult)
+    {
+        if (index >= digits.Length)
+        {
+            result.Add(tempResult);
+            return;
+        }
+        var letters = letterDic[digits[index]];
+        foreach (var letter in letters)
+        {
+            Backtrace(digits,index+1, letterDic, result, tempResult+letter);
+        }
     }
 }
 // @lc code=end
--- VSCode_Practice/Program.cs
+++ VSCode_Practice/Program.cs
@@ -10,7 +10,7 @@ namespace study
         private static int[] _nums = new int[1000];
         static void Main(string[] args)
         {
-            var sol = new Solution010();
+            var sol = new Solution017();
             sol.Test(); 
 
             //aggregate用法测试
