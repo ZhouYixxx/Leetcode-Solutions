463923d829a2d3e3ffd130a601230317904bb0e0
zhouyi <zhouyi9211@163.com>
2023-06-01 17:47:19 +0800

    2023-06-01复健训练

--- VSCode_Practice/473.火柴拼正方形.cs
+++ VSCode_Practice/473.火柴拼正方形.cs
@@ -59,6 +59,44 @@ public class Solution473 {
         }
         return false;
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="index">loop from n-1 to -1, n is the count of matchStickes</param>
+    /// <param name="edgeLength">the edge length of square</param>
+    /// <param name="edges">the array contains four edge length of square</param>
+    /// <param name="matches">the match array</param>
+    /// <returns></returns>
+    private bool DFS_Ver2(int index, int edgeLength, int[] edges, int[] matches)
+    {
+        if (index < 0)
+        {
+            return edges[0] == edges[1] && edges[1] == edges[2] && edges[2] == edges[3];
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            //剪枝条件1：edges[i] + matches[index] > edgeLength，说明matches[index]不能放在edges[i]下，直接跳过
+            if (edges[i] + matches[index] > edgeLength)
+            {
+                continue;
+            }
+            //剪枝条件2：既然循环走到了edges[i]，说明edges[i-1]的尝试已经失败了,因为DFS是一条道走到底的，
+            //因此如果edges[i] == edges[i-1]，edges[i]必然也无法成功，直接跳过
+            if (i > 0 && edges[i] == edges[i-1])
+            {
+                continue;
+            }
+            edges[i] += matches[index];
+            var isValid = DFS_Ver2(index-1, edgeLength, edges, matches);
+            if (isValid)
+            {
+                return true;
+            }
+            edges[i] -= matches[index];
+        }
+        return false;
+    }
 }
 // @lc code=end
 
