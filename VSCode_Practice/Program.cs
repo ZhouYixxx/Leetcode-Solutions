d85ac6bfcab2fb857c7b203469dbb8db2ee65a79
zhouyi <zhouyi9211@163.com>
2023-06-01 17:47:19 +0800

    2023-06-01复健训练

--- VSCode_Practice/Program.cs
+++ VSCode_Practice/Program.cs
@@ -16,7 +16,7 @@ namespace VSCode_Practice
             // AsyncTest();
             // System.Console.WriteLine($"222, My Thread ID is :{Thread.CurrentThread.ManagedThreadId}");
             //Console.ReadKey();
-            var sol = new Solution003();
+            var sol = new Solution473();
             sol.Test(); 
 
             //aggregate用法测试
